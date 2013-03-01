6bb3391df1f91b931ef9359b5b43f4669f99c8e3
Kevin Tong <logicmd@gmail.com>
2013-03-02 00:00:10 +0800

    WIP

--- BestTimetoBuyandSellStockIII.java
+++ BestTimetoBuyandSellStockIII.java
@@ -9,17 +9,19 @@ public class BestTimetoBuyandSellStockIII {
 
         int n = prices.length;
 
-        int[] savedMin = new int[2];
-        int[] savedSaleTime = new int[2];
+        int[] savedMin = {Integer.MAX_VALUE, Integer.MAX_VALUE};
+        int[] savedSaleTime = {-1, -1};
         int[] savedProfit = new int[2];
+        int total = 0;
+        int savedBuyTime = 0;
 
-        for (int mid=1; mid<n-1; mid++) {
+        for (int mid=0; mid<n; mid++) {
             int profitCandidate = 0;
-            int saleTime = savedSaleTime[0]；
+            int saleTime = savedSaleTime[0];
             int min = savedMin[0];
-            int profit = savedProfit[0];
+            int profit = -1;
 
-            for (int i=saleTime + 1; i<=mid; i++) {
+            for (int i=saleTime+1; i<=mid; i++) {
                 if (min > prices[i]) {
                     min = prices[i];
                 }
@@ -34,33 +36,43 @@ public class BestTimetoBuyandSellStockIII {
             savedMin[0] = min;
             savedProfit[0] = profit;
 
+            int buyTime = savedBuyTime >= mid ? savedBuyTime : mid;
             profitCandidate = 0;
-            saleTime = savedSaleTime[1] < mid + 1 ? mid + 1 : savedSaleTime[1]；
-            min = savedMin[1];
-            profit = savedProfit[1];
+            saleTime = mid - 1;
+            min = Integer.MAX_VALUE;
+            profit = -1;
 
-            for (int i=saleTime + 1; i<n; i++) {
+            for (int i=buyTime; i<n; i++) {
                 if (min > prices[i]) {
                     min = prices[i];
+                    buyTime = i;
                 }
                 profitCandidate = prices[i] - min;
                 if (profitCandidate > profit) {
                     profit = profitCandidate;
                     saleTime = i;
+                    savedBuyTime = buyTime;
                 }
             }
 
+            savedSaleTime[1] = saleTime;
+            savedMin[1] = min;
+            savedProfit[1] = profit;
+
+            if(total < savedProfit[0] + savedProfit[1]) {
+                total = savedProfit[0] + savedProfit[1];
+            }
+
         }
+        //System.out.println(savedMin[0] + " " + savedSaleTime[0] +
+        //    " " + savedMin[1] + " " + savedSaleTime[1]);
+        return total;
     }
 
-    void updateMinus(int[] minus, int profit) {
-        if (profit > minus[0]) {
-            if (profit > minus[1]) {
-                minus[0] = minus[1];
-                minus[1] = profit;
-            } else {
-                minus[0] = profit;
-            }
-        }
+    public static void main(String[] args) {
+        BestTimetoBuyandSellStockIII b = new BestTimetoBuyandSellStockIII();
+        int[] prices = {1,2};
+        System.out.println(b.maxProfit(prices));
     }
+
 }
