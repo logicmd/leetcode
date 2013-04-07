a0424c6e272181d06acb532a2dc9b7a6c87a439c
Kevin Tong <logicmd@gmail.com>
2013-04-07 23:19:06 +0800

    WIP

--- MinimumWindowSubstring.cpp
+++ MinimumWindowSubstring.cpp
@@ -7,7 +7,8 @@
 #include <string>
 #include <utility>
 #include <algorithm>
-#include <unordered_set>
+#include <multiset>
+#include <multimap>
 #include <stack>
 
 using namespace std;
@@ -16,11 +17,60 @@ public:
     string minWindow(string S, string T) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        unordered_set<char> hashset;
-        for (int i = 0; i < T.size(); ++i)
+        multiset<char> hashset;
+        multimap<char, int> hashmap;
+        int n = T.size();
+        if (n == 0)
+        {
+            return "";
+        }
+        for (int i = 0; i < n; ++i)
         {
             hashset.insert(T[i]);
         }
+
+        int s = S.size() - 1, e = 0;
+        if (s == -1)
+        {
+            return "";
+        }
+        for (int i = 0; i < S.size(); ++i)
+        {
+            if (hashset.find(S[i]) != hashset.end())
+            {
+                hashmap.insert(make_pair(S[i], i));
+            }
+            if (hashmap.size() == n)
+            {
+                int min = S.size();
+                int max = -1;
+                for (multimap<char, int>::iterator j = hashmap.begin(); j != hashmap.end(); ++j)
+                {
+                    if (j->second > max)
+                    {
+                        max = j->second;
+                    }
+                    if (j->second < min)
+                    {
+                        min = j->second;
+                    }
+                }
+
+                if (max - min > 0 && max - min <= S.size() - 1 && max - min < e - s)
+                {
+                    e = max;
+                    s = min;
+                }
+            }
+        }
+        if (s <= e)
+        {
+            return S.substr(s, e-s+1);
+        }
+        return "";
+
+
+
     }
 };
 
