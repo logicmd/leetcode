499cf04498dd6568555c7123e40f32aa0baf6934
Kevin Tong <logicmd@gmail.com>
2013-05-26 17:32:53 +0800

    wip

--- PalindromePartitioning.cpp
+++ PalindromePartitioning.cpp
@@ -1,6 +1,17 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cstdio>
+#include <string>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
-    bool isPrlindrome(string s, int i, int j) {
+    bool isPrindrome(string s, int i, int j) {
         bool flag = true;
         while (i > j) {
             if(s[i] == s[j]) {
@@ -13,15 +24,27 @@ public:
         return flag;
     }
 
-    void dfs(string s, int i, int j, vector<string> &cur, vector<vector<string>> &set) {
+    void dfs(string s, int i, vector<string> &cur, vector<vector<string>> &set) {
         int n = s.size();
-        for(int i = 0; ; i<n) {
-            for(int j = i; j++; j<n) {
-                if(isPrindrome(s, i, j)) {
-                    cur.push_back(s.substr(i, j));
+
+        if (i >= n)
+        {
+            return;
+        }
+
+        for(int j = i+1; j<n; ++j) {
+            if(isPrindrome(s, i, j)) {
+                cur.push_back(s.substr(i, j));
+                if (j >= n) {
+                    break;
+                } else if (j == n-1)
+                {
+                    set.push_back(cur);
                 }
+
+                dfs(s, j+1, cur, set);
+                cur.erase(--cur.end());
             }
-            i = j+1;
         }
 
 
@@ -30,6 +53,9 @@ public:
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         vector<vector<string>> set;
+        vector<string> cur;
 
+        dfs(s, 0, cur, set);
+        return set;
     }
 };
