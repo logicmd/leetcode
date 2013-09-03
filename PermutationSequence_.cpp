eb12b4a9c6c9a222d3f628a25b23f332a7f8b4eb
Kevin Tong <logicmd@gmail.com>
2013-09-04 00:58:25 +0800

    wip

--- PermutationSequence_.cpp
+++ PermutationSequence_.cpp
@@ -8,12 +8,15 @@ public:
         for(int i=1; i<=n-1; ++i) {
             frac*=i;
         }
-
+        k--;
         string s;
         for(int i=n-1; i>=0; --i) {
-            int t=k%frac;
-            char c=t+'1';
-            s.insert(s.begin(), c);
+            int t=k/frac;
+            int offset=0;
+            if(k%frac==0&&k==1)
+                offset=1;
+            char c=t-offset+'1';
+            s.insert(s.end(), c);
             k-=t*frac;
             if(i!=0)
                 frac/=i;
