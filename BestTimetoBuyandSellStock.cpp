f10bc9a8ce406eb26a41d62091dda9cc7ac5d35d
Kevin Tong <logicmd@gmail.com>
2013-09-08 00:18:40 +0800

    ac

--- BestTimetoBuyandSellStock.cpp
+++ BestTimetoBuyandSellStock.cpp
@@ -1,24 +1,41 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cstdio>
+#include <string>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         int n=prices.size();
-        if(n==0)    return 0;
-        if(n==1)    return prices[0];
-        int min = 1<<30 -1 + 1<<30;
+        if(n==0 || n==1)    return 0;
+        int min = (1<<30 - 1)+1<<30; // 坑
         int max = -1<<31;
 
         int re=0;
 
-        for(int i=0; i<prices.size(); i++) {
-            if(prices[i]<min)
+        for(int i=0; i<n; i++) {
+            if(prices[i]<min) {
                 min=prices[i];
-            if(prices[i]>max)
-                max=prices[i];
+                max=0;
+            }
 
-            if(max-min>re)
+            if(prices[i]>max) {
+                max=prices[i];
+            }
+
+
+            if(max-min>re) {
                 re=max-min;
+            }
+
 
         }
 
@@ -26,3 +43,16 @@ public:
 
     }
 };
+
+
+
+int main()
+{
+    Solution s;
+    int a[] = {1,2};
+    vector<int> v(a, a+2);
+    cout << s.maxProfit(v);
+
+    system("PAUSE");
+    return 0;
+}
