1ccba1cfc49e1b54d68d0ed8ff6b405dec7f5237
Kevin Tong <logicmd@gmail.com>
2013-09-08 01:16:10 +0800

    wip

--- LongestValidParentheses_.cpp
+++ LongestValidParentheses_.cpp
@@ -14,13 +14,13 @@ public:
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         stack<node> st;
-        int re;
+        int re=0;
         for(int i=0; i<s.size(); i++) {
-            if(st.empty() && s[i] == '(') {
+            if(st.empty()) {
                 st.push(node(s[i], i));
             } else {
                 if(s[i]==')') {
-                    node top = st.front();
+                    node top = st.top();
                     if(top.c=='(') {
                         st.pop();
                         re=max(re, top.i-i+1);
