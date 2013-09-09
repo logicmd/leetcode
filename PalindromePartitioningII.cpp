6f6e032baa5da42c93aacc17b5b1e5f9cee5b1f2
Kevin Tong <logicmd@gmail.com>
2013-09-10 01:24:57 +0800

    wip

--- PalindromePartitioningII.cpp
+++ PalindromePartitioningII.cpp
@@ -12,8 +12,8 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPrindrome(string s, int i, int j, map<string, bool> &m) {
-        map<string, bool>::iterator got = m.find(s.substr(i, j-i+1));
+    bool isPrindrome(string s, int i, int j, map< pair<int, int>, bool> &m) {
+        map<pair<int, int>, bool>::iterator got = m.find(make_pair(i, j));
         if(got != m.end()) {
             return got->second;
         }
@@ -24,58 +24,49 @@ public:
         int j_copy = j;
         while (i <= j) {
             if(s[i] == s[j]) {
-                i++;
-                j--;
-                map<string, bool>::iterator _got = m.find(s.substr(i, j-i+1));
+
+                map<pair<int, int>, bool>::iterator _got = m.find(make_pair(i, j));
+
                 if(_got != m.end()) {
                     return _got->second;
+
                 }
+                i++;
+                j--;
             } else {
                 flag = false;
                 break;
             }
         }
-        m.insert(make_pair(s.substr(i_copy, j_copy-i_copy+1), flag));
+        m.insert(make_pair(make_pair(i_copy, j_copy), flag));
         return flag;
     }
 
-    void dfs(string s, int i, vector<string> &cur, set<int> &st) {
-        int n = s.size();
-
-        map<string, bool> m;
-
-        if (i >= n)
-        {
-            return;
-        }
-
-        for(int j = i; j<n; ++j) {
-            if(isPrindrome(s, i, j, m)) {
-                cur.push_back(s.substr(i, j-i+1));
-                if (j == n-1) {
-                    st.insert(cur.size());
-                }
-
-                dfs(s, j+1, cur, st);
-                cur.erase(--cur.end());
-            }
-        }
-
-
-    }
 
     int minCut(string s) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        set<int> st;
-        vector<string> cur;
+        int n=s.size();
+        if(n<=1)    return 0;
+        int *dp = new int[n];
+        map<pair<int, int>, bool> m;
+        dp[0]=1;
+        for(int i=1; i<n; i++) {
+            int min=2147483647;
+            if(isPrindrome(s, 0, i, m)) {
+                dp[i]=1;
+                continue;
+            }
+            for(int j=0; j<i; j++) {
+                if(isPrindrome(s, j+1, i, m)) {
+                    if(1+dp[j]<min)
+                        min = 1+dp[j];
+                }
+            }
+            dp[i]=min;
+        }
 
 
-        //map<pair<int i, int j>, bool> m;
-        //vector<vector<bool>> v;
-
-        dfs(s, 0, cur, st);
-
-        return *(st.begin())-1;
+        return dp[n-1]-1;
     }
 };
